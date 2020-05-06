a21de60e8627354c306f30f73792ef8f5b9518c0
Spencer Neveux <33857808+spencerneveux@users.noreply.github.com>
2020-05-06 14:53:11 -0700

    Fixing settings.py backend auth

--- thevalidator/settings.py
+++ thevalidator/settings.py
@@ -144,7 +144,8 @@ STATIC_URL = '/static/'
 AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
     'social_core.backends.github.GithubOAuth2',
-    'social_core.backends.google.GoogleOAuth2'
+    'social_core.backends.google.GoogleOAuth2',
+    'social_core.backends.facebook.FacebookOAuth2'
 )
 
 LOGIN_URL = 'login'
